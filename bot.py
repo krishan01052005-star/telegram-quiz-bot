770ab64f8a989aefa86166945e7d77cad6b14cc1
krishan01052005-star <krishan01052005@gmail.com>
2025-12-08 20:59:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,11 @@ application = ApplicationBuilder().token(TOKEN).build()
 
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 8443))
-    application.run_webhook(
+
+    application.run_web_app(
         listen="0.0.0.0",
         port=PORT,
         url_path=TOKEN,
         webhook_url=f"https://telegram-quiz-bot-pbts.onrender.com/{TOKEN}"
     )
+
