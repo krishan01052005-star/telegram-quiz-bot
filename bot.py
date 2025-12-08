ce0beb26386f8fd919d3ae11c505c866e8093f14
krishan01052005-star <krishan01052005@gmail.com>
2025-12-08 21:19:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,21 @@ from telegram import Update
 from telegram.ext import ContextTypes
 import os
 
-TOKEN = os.getenv("8279748377:AAFnf8irohjfmRn3tYyrYf9sqNgIUiGrPIA")  # ENV variable se token lo
+TOKEN = os.getenv("8279748377:AAFnf8irohjfmRn3tYyrYf9sqNgIUiGrPIA")
+print("TOKEN FROM ENV:", TOKEN)  # Debug line
+
+if not TOKEN:
+    raise ValueError("❌ BOT_TOKEN missing in Render Environment Variables!")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is Running! 🎯")
+    await update.message.reply_text("Bot: I'm alive! 🚀")
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
-
     application.add_handler(CommandHandler("start", start))
 
     PORT = int(os.environ.get("PORT", 8443))
-    
+
     application.run_webhook(
         listen="0.0.0.0",
         port=PORT,
@@ -24,4 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
